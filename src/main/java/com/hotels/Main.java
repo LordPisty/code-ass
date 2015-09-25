d3761a4acd44cf8ac1b64791c6072ebdf9d15c61
screspi <stefano.crespi.85@gmail.com>
2015-09-25 17:58:23 -0500

    Added main and dataset.

---
+++ src/main/java/com/hotels/Main.java
@@ -0,0 +1,11 @@
+package com.hotels;
+
+/**
+ * Created by screspi on 9/25/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
