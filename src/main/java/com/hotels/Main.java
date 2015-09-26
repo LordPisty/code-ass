dccbde31124bd42634be87f0e2273f510750c685
screspi <stefano.crespi.85@gmail.com>
2015-09-25 19:50:53 -0500

    Reading from file.

--- src/main/java/com/hotels/Main.java
+++ src/main/java/com/hotels/Main.java
@@ -1,17 +1,31 @@
 package com.hotels;
 
-import com.hotels.domain.Location;
-import com.hotels.input.FileLocationSupplier;
+import com.hotels.input.ClasspathFileLocationSupplier;
 import com.hotels.input.LocationSupplier;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Created by screspi on 9/25/15.
  */
 public class Main {
 
+    private static final String PROPERTIES_FILE = "/config.properties";
+
+    private static final String DATASET_PATH = "dataset.path";
+
     public static void main(String[] args) {
 
-        final LocationSupplier locationSupplier = new FileLocationSupplier();
+        Properties prop = new Properties();
+        try (InputStream input = Main.class.getResourceAsStream(PROPERTIES_FILE)) {
+            prop.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        final LocationSupplier locationSupplier = new ClasspathFileLocationSupplier(prop.getProperty(DATASET_PATH));
         System.out.println(locationSupplier.get());
 
     }
--- src/main/java/com/hotels/input/ClasspathFileLocationSupplier.java
+++ src/main/java/com/hotels/input/ClasspathFileLocationSupplier.java
@@ -2,6 +2,7 @@ package com.hotels.input;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hotels.domain.Location;
+import org.apache.commons.lang3.Validate;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,18 +10,23 @@ import java.io.IOException;
 /**
  * Created by screspi on 9/25/15.
  */
-public class FileLocationSupplier implements LocationSupplier {
+public class ClasspathFileLocationSupplier implements LocationSupplier {
 
-    @Override
-    public Location get() {
-        final File file = new File(this.getClass().getResource("/com/hotels/dataset.json").getFile());
+    private Location location = null;
+
+    public ClasspathFileLocationSupplier(String filePath) {
+        Validate.notBlank(filePath);
+        final File file = new File(this.getClass().getResource(filePath).getFile());
         final ObjectMapper objMapper = new ObjectMapper();
-        Location location = null;
         try {
             location = objMapper.readValue(file, Location.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public Location get() {
         return location;
     }
 }
